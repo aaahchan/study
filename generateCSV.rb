3c05e846f06dd888756b71fca64f7e0f3ce91c91
aaahchan <51350160+aaahchan@users.noreply.github.com>
2020-04-08 20:22:41 +0900

    NULLの場合の処理を追加

--- generateCSV.rb
+++ generateCSV.rb
@@ -1,9 +1,15 @@
 require 'roo'
 
 xlsx = Roo::Excelx.new(Dir.pwd + '/Initial_Data.xlsx')
-
-  sheetName = xlsx.sheets
+sheetName = xlsx.sheets
   
   sheetName.each{|i|
-  xlsx.sheet(i).to_csv(i + '.csv')
+    #create csv file
+    fileName = i + '.csv'
+    xlsx.sheet(i).to_csv(fileName)
+
+    #replace "NULL" to NULL
+    buffer = File.open(Dir.pwd + '/' + fileName){|f| f.read()}
+    buffer.gsub!('"NULL"','NULL')
+    File.open(Dir.pwd + '/' + fileName, "w") { |f| f.write(buffer) }
   }
